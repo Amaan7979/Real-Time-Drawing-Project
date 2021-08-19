75131f15920e95953f42a766e4105bb8230fbbac
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-08-19 17:33:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 function setup() {
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(350,350);
+    video.position(175,155);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",got_results);
+}
+function draw() {
+    background("blue");
+    
+}
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+function got_results(results) {
+if(results.length>0){
+console.log(results);
+}
 }
